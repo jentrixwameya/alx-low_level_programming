adf2944108ab5fd5e0c3cdd77613d007092444ac
jentrixwameya <jentrixwameya@gmail.com>
2022-06-30 19:46:40 -0700

    Hello put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,35 +1,9 @@
-#include <stdio.h>
-
-
-
-/**
+/**                                                                 
  *
- *  * main - Write '"Programming is like building a multilingual puzzle'
+ *  * main - Entry point                                               
  *
- *   *
+ *   *                                                                  
  *
- *    * *Return: Always 0 (success)
+ *    * Return: Always 0 (Success)                                       
  *
- *     *
- *
- *      */
-
-
-
-int main(void)
-
-
-
-{
-
-
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-
-
-			return (0);
-
-
-
-}
+ *    */
