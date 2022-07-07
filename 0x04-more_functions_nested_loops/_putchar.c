5ce56890bd34a933017c2f5f34f09b1d381c43b6
jentrixwameya <jentrixwameya@gmail.com>
2022-07-07 15:29:36 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+/**
+ * _putchar -  writes the character to stdout
+ *
+ * @c: character to be printed
+ *
+ * Return: on success 1
+ *
+ * on error, -1 is returned, and error is set approximately
+ */
+
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+
+}
